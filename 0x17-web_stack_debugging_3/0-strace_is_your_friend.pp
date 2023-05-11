f4c08b2ec3bc13c0964a49864bd54aaa19141f27
Fatumayattani <fyattani@gmail.com>
2023-05-11 00:21:06 -0700

    update file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,49 +1,6 @@
-# Install strace package
-package { 'strace':
-  ensure => installed,
-}
+# Puppet manifest to fix a bug in wp-setings.php
 
-# Attach strace to Apache process
-exec { 'attach_strace_to_apache':
-  command => 'strace -p $(pgrep apache)',
-  path    => '/usr/bin',
-  creates => '/tmp/strace_output.txt',
-}
-
-# Simulate the 500 error using curl
-exec { 'simulate_500_error':
-  command     => 'curl http://localhost/',
-  path        => '/usr/bin',
-  refreshonly => true,
-  subscribe   => Exec['attach_strace_to_apache'],
-}
-
-# Analyze strace output for Apache error
-exec { 'analyze_strace_output':
-  command     => 'grep "ERROR" /tmp/strace_output.txt > /tmp/apache_error.txt',
-  path        => '/usr/bin',
-  refreshonly => true,
-  subscribe   => Exec['simulate_500_error'],
-}
-
-# Fix the Apache issue - Replace with your specific fix
-file { '/path/to/your/apache/config.conf':
-  ensure  => present,
-  content => 'Your Apache configuration content',
-  notify  => Service['apache'],
-}
-
-# Restart Apache service
-service { 'apache':
-  ensure  => running,
-  enable  => true,
-  require => File['/path/to/your/apache/config.conf'],
-}
-
-# Apply the Puppet manifest
-exec { 'apply_puppet_manifest':
-  command => 'puppet apply 0-strace_is_your_friend.pp',
-  path    => '/usr/bin',
-  creates => '/tmp/puppet_manifest_applied',
-  require => Service['apache'],
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
